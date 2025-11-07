419175f63808193d1d23c75307d7f518acbd63cb
이찬해 <chanhae1020@nis.co.kr>
2025-11-07 16:59:18 +0900

    init: quiz-back project

---
+++ src/main/java/ch/quizback/QuizBackApplication.java
@@ -0,0 +1,13 @@
+package ch.quizback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/quizback/QuizBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.quizback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
